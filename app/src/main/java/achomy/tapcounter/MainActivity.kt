58c6b806dfa6840348645d38f34cd7fccbbdf176
amoschoomy <acho0057@student.monash.edu>
2021-01-26 19:46:34 +0800

    Configured menu items on action bar. Not yet complete

--- app/src/main/java/achomy/tapcounter/MainActivity.kt
+++ app/src/main/java/achomy/tapcounter/MainActivity.kt
@@ -1,9 +1,15 @@
 package achomy.tapcounter
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.MenuItemCompat
+import androidx.core.view.MenuItemCompat.getActionView
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         taps += 1
         val counter: TextView = findViewById(R.id.counter)
         counter.text = "Taps: $taps"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu);
+        val nightMode= menu?.findItem(R.id.app_bar_switch)
+        val actionView:SwitchCompat= nightMode?.actionView as SwitchCompat;
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
