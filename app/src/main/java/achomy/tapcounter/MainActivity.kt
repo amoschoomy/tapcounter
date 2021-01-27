dc5cd3031368f63627b916ceadd0a7c1a65d5605
amoschoomy <acho0057@student.monash.edu>
2021-01-27 18:16:47 +0800

    Added night mode toggle

--- app/src/main/java/achomy/tapcounter/MainActivity.kt
+++ app/src/main/java/achomy/tapcounter/MainActivity.kt
@@ -1,15 +1,16 @@
 package achomy.tapcounter
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.view.MenuItemCompat
-import androidx.core.view.MenuItemCompat.getActionView
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,8 +28,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.app_menu, menu);
-        val nightMode= menu?.findItem(R.id.app_bar_switch)
-        val actionView:SwitchCompat= nightMode?.actionView as SwitchCompat;
+        val nightMode = menu?.findItem(R.id.app_bar_switch)
+        val actionView: SwitchCompat = nightMode?.actionView as SwitchCompat
+        val toolbar:Toolbar = findViewById(R.id.my_toolbar)
+        val menuLayout: ConstraintLayout = findViewById(R.id.menuLayout)
+        actionView.text = "Night Mode"
+        actionView.setOnClickListener(View.OnClickListener {
+            when (actionView.isChecked) {
+                true -> {
+                    menuLayout.setBackgroundColor(ContextCompat.getColor(applicationContext,
+                            R.color.black));
+                }
+                false -> menuLayout.setBackgroundColor(ContextCompat.getColor(applicationContext,
+                        R.color.white))
+
+            }
+        })
         return super.onCreateOptionsMenu(menu)
     }
 
