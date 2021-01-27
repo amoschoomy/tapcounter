531bae825c50ec3f77cb082c8f3b9fc85e346615
amoschoomy <acho0057@student.monash.edu>
2021-01-27 19:14:32 +0800

    Added reset counter to the app

--- app/src/main/java/achomy/tapcounter/MainActivity.kt
+++ app/src/main/java/achomy/tapcounter/MainActivity.kt
@@ -1,14 +1,11 @@
 package achomy.tapcounter
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.TextView
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
-import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 
@@ -21,16 +18,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun screenTap(view: View) {
-        taps += 1
-        val counter: TextView = findViewById(R.id.counter)
-        counter.text = "Taps: $taps"
+       editTaps(1,false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.app_menu, menu);
         val nightMode = menu?.findItem(R.id.app_bar_switch)
         val actionView: SwitchCompat = nightMode?.actionView as SwitchCompat
-        val toolbar:Toolbar = findViewById(R.id.my_toolbar)
         val menuLayout: ConstraintLayout = findViewById(R.id.menuLayout)
         actionView.text = "Night Mode"
         actionView.setOnClickListener(View.OnClickListener {
@@ -44,7 +38,17 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+        menu?.findItem(R.id.reset)?.setOnMenuItemClickListener {editTaps(0,true);true}
         return super.onCreateOptionsMenu(menu)
+    }
+    fun editTaps(value: Int,reset:Boolean){
+        if (!reset){
+        taps+=value}
+        else{
+            taps=0
+        }
+        val counter: TextView = findViewById(R.id.counter)
+        counter.text = "Taps: $taps"
     }
 
 }
